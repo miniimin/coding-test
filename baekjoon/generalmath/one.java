1bde024657054a7fc368aa71945328f96f309aa8
sunmin <jsunmin0@gmail.com>
2024-11-29 16:39:06 +0900

    일반 수학 1

--- baekjoon/generalmath/one.java
+++ baekjoon/generalmath/one.java
@@ -3,8 +3,27 @@ package generalmath;
 import java.io.*;
 
 public class one {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int b = Integer.parseInt(input.split(" ")[1]);
+        String n = input.split(" ")[0];
 
+        int sum = 0;
+
+        for(int i = 0; i < n.length(); i++) {
+            sum += charToInt(n.charAt(n.length() - i - 1)) * (int)Math.pow(b, i);
+        }
+        System.out.println(sum);
+    }
+
+    static int charToInt(char c) {
+        int num;
+        if (c >= 'A' && c <= 'Z'){
+            num = c - 'A' + 10;
+        } else {
+            num = c - '0';
+        }
+        return num;
     }
 }
