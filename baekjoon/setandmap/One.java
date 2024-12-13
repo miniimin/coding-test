f7addb5c7a062e4de3290fa47cc2d67fbca53da0
unknown <jsunmin0@gmail.com>
2024-12-13 15:41:47 +0900

    집합과 맵

---
+++ baekjoon/setandmap/One.java
@@ -0,0 +1,31 @@
+package setandmap;
+
+import java.io.*;
+import java.util.*;
+
+public class One {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Set<Integer> set = new HashSet<>();
+
+        while (n-- > 0) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+
+        for (int i = 0; i < m; i++) {
+            int x = Integer.parseInt(st2.nextToken());
+            sb.append(set.contains(x)? 1 : 0).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
