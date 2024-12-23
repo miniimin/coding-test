0360ef1f87eda7a524ed695dbcbe7cd49c1f6a3c
sunmin <jsunmin0@gmail.com>
2024-12-23 13:29:29 +0900

    약수, 배수와 소수2

--- baekjoon/divisormultipledemical2/Five.java
+++ baekjoon/divisormultipledemical2/Five.java
@@ -3,19 +3,27 @@ package divisormultipledemical2;
 import java.io.*;
 
 public class Five {
+
+    public static boolean isPrime(long n) {
+        if (n < 2)  return false;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            long temp = Long.parseLong(br.readLine());
+            while(!isPrime(temp)) {
+                temp++;
+            }
+            sb.append(temp).append("\n");
         }
-
-
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
