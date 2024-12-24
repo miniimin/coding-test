8df95e19029c86bb2ec0a6f38cef98b676d070d9
sunmin <jsunmin0@gmail.com>
2024-12-24 19:07:34 +0900

    약수, 배수와 소수 2

---
+++ baekjoon/divisormultipledemical2/Eight.java
@@ -0,0 +1,16 @@
+package divisormultipledemical2;
+
+import java.io.*;
+import java.util.*;
+
+public class Eight {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            
+        }
+    }
+}
