a3358c370a22941f9897eeb2533de60a7e253ea3
sunmin <jsunmin0@gmail.com>
2024-12-30 11:24:38 +0900

    약수, 배수와 소수 2

--- baekjoon/divisormultipledemical2/Eight.java
+++ baekjoon/divisormultipledemical2/Eight.java
@@ -4,14 +4,36 @@ import java.io.*;
 import java.util.*;
 
 public class Eight {
+
     public static void main(String[] args) throws IOException {
+        boolean[] arr = new boolean[1000001];
+        Arrays.fill(arr, true);
+        arr[0] = arr[1] = false;
+
+        for (int i = 2; i * i<= 1000000; i++) {
+            if (arr[i]) {
+                for (int j = i * i; j <= 1000000; j += i) {
+                    arr[j] = false;
+                }
+            }
+        }
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            
+            int N = Integer.parseInt(br.readLine());
+            int count = 0;
+
+            for (int i = 2; i <= N / 2; i++) {
+                if (arr[i] && arr[N - i]) {
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
         }
+
+        System.out.print(sb);
     }
 }
