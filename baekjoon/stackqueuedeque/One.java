82b885b611d6536e01a6da8af92787495e998e78
sunmin <jsunmin0@gmail.com>
2024-12-31 15:22:24 +0900

    스택

--- baekjoon/stackqueuedeque/One.java
+++ baekjoon/stackqueuedeque/One.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 public class One {
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
     }
 }
