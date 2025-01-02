76126af86717141d9b45df05d3cec6bfdf454936
sunmin <jsunmin0@gmail.com>
2025-01-02 14:03:23 +0900

    스택

--- baekjoon/stackqueuedeque/One.java
+++ baekjoon/stackqueuedeque/One.java
@@ -7,6 +7,24 @@ public class One {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        while (n-- > 0) {
+            int i = Integer.parseInt(br.readLine());
+            switch (i) {
+                case 1 :
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+
+        }
 
 
     }
