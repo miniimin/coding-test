98b372110b03a014dc019ed65fd2de08a2ad1992
sunmin <jsunmin0@gmail.com>
2025-01-05 16:10:51 +0900

    스택

--- baekjoon/stackqueuedeque/Two.java
+++ baekjoon/stackqueuedeque/Two.java
@@ -1,4 +1,26 @@
 package stackqueuedeque;
 
+import java.io.*;
+import java.util.*;
+
 public class Two {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        int k = Integer.parseInt(br.readLine());
+
+        while(k-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+        while(!stack.empty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
 }
