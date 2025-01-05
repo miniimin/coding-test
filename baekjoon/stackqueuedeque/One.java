5d0cba86b9fd7fb0d866f953a7121340607078c3
sunmin <jsunmin0@gmail.com>
2025-01-05 15:53:23 +0900

    스택

--- baekjoon/stackqueuedeque/One.java
+++ baekjoon/stackqueuedeque/One.java
@@ -6,26 +6,43 @@ import java.util.*;
 public class One {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
 
         while (n-- > 0) {
-            int i = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
             switch (i) {
                 case 1 :
+                    stack.push(Integer.parseInt(st.nextToken()));
                     break;
                 case 2:
+                    if(stack.empty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
                     break;
                 case 3:
+                    sb.append(stack.size()).append("\n");
                     break;
                 case 4:
+                    if(stack.empty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
                     break;
                 case 5:
+                    if(stack.empty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.get(stack.size() - 1)).append("\n");
+                    }
                     break;
             }
-
         }
-
-
+        System.out.println(sb);
     }
 }
---
+++ baekjoon/stackqueuedeque/Two.java
@@ -0,0 +1,4 @@
+package stackqueuedeque;
+
+public class Two {
+}
